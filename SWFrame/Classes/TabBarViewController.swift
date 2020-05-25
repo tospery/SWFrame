fa32038c0a35bed76c285cf8ccd4c0eeeaa7b08b
YangJianxiang <tospery@gmail.com>
2020-05-25 17:05:15 +0800

    TabBar

--- SWFrame/Classes/TabBarViewController.swift
+++ SWFrame/Classes/TabBarViewController.swift
@@ -9,20 +9,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 import URLNavigator
+import ESTabBarController_swift
 
 open class TabBarViewController: ScrollViewController {
     
-    lazy public var safeBottomView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .clear
-        view.isHidden = !isNotchedScreen
-        view.sizeToFit()
-        return view
-    }()
+//    lazy public var safeBottomView: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = .green
+//        view.isHidden = !isNotchedScreen
+//        view.sizeToFit()
+//        return view
+//    }()
 
-    public let tab: UITabBarController = {
-        let tabBarController = UITabBarController()
-        tabBarController.tabBar.isTranslucent = false
+    public let tab: ESTabBarController = {
+        let tabBarController = ESTabBarController()
+        // tabBarController.tabBar.isTranslucent = false
         return tabBarController
     }()
     
@@ -42,8 +43,8 @@ open class TabBarViewController: ScrollViewController {
         self.tab.view.frame = self.contentFrame
         self.tab.didMove(toParent: self)
 
-        self.view.addSubview(self.safeBottomView)
-        self.safeBottomView.frame = CGRect(x: 0, y: self.tab.view.bottom, width: self.view.width, height: safeBottom)
+//        self.view.addSubview(self.safeBottomView)
+//        self.safeBottomView.frame = CGRect(x: 0, y: self.tab.view.bottom, width: self.view.width, height: safeBottom)
     }
 
     open override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +53,10 @@ open class TabBarViewController: ScrollViewController {
 
     open override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+    }
+
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
     
     open override var shouldAutorotate: Bool {
@@ -68,3 +73,29 @@ open class TabBarViewController: ScrollViewController {
     
 }
 
+//open class TabBarController: UITabBarController {
+//
+//    open override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    open override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//    }
+//
+//    open override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//    }
+//
+//    open override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+////        if #available(iOS 11.0, *), isNotchedScreen {
+////            for view in self.view.subviews {
+////                if let tabBar = view as? UITabBar {
+////                    tabBar.frame = CGRect(x: tabBar.frame.origin.x, y: tabBar.frame.origin.y, width: tabBar.frame.size.width, height: tabBarHeight)
+////                }
+////            }
+////        }
+//    }
+//
+//}
