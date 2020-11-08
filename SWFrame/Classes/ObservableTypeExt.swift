0be3ff34d0a2902ad2279f68012569bd068c406e
yangjianxiang <tospery@gmail.com>
2020-11-08 11:25:44 +0800

    retry

--- SWFrame/Classes/ObservableTypeExt.swift
+++ SWFrame/Classes/ObservableTypeExt.swift
@@ -10,6 +10,19 @@ import RxSwift
 import Moya
 import ObjectMapper
 
+extension ObservableType {
+    
+    public func retry(maxAttempts: UInt, delay: RxTimeInterval) -> Observable<Element> {
+        self.retryWhen { error in
+            error.enumerated().flatMap { (index, error) -> Observable<Int> in
+                guard (index + 1) < maxAttempts else { return .error(error) }
+                return Observable<Int>.timer(delay, scheduler: MainScheduler.instance)
+            }
+        }
+    }
+    
+}
+
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 public extension ObservableType where Element == Response {
 
