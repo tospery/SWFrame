a26a35bf7b266527610f46b4fae4a0aef74948c8
yangjianxiang <yangjianxiang@xunyou.com>
2020-11-09 18:16:31 +0800

    整理

--- SWFrame/Classes/ModelType.swift
+++ SWFrame/Classes/ModelType.swift
@@ -19,20 +19,7 @@ private var streams: [String: Any] = [:]
 public protocol Identifiable {
     associatedtype Identity: Hashable
     var id: Identity { get }
-    
-    // var id: String { get }
 }
-
-//public extension Identifiable {
-//    var id: String {
-//        get {
-//            id
-//        }
-//        set {
-//            id = newValue
-//        }
-//    }
-//}
 
 // MARK: - 模型协议
 public protocol ModelType: Mappable, CustomStringConvertible {
@@ -64,6 +51,16 @@ public struct BaseModel: ModelType {
     public mutating func mapping(map: Map) {
     }
     
+}
+
+public struct ModelContext: MapContext {
+    
+    public let shouldMap: Bool
+    
+    public init(shouldMap: Bool = true){
+        self.shouldMap = shouldMap
+    }
+
 }
 
 // MARK: - 事件协议
@@ -142,118 +139,4 @@ public extension Storable {
 
 }
 
-//public extension Storable2 {
-//
-//    func save(ignoreKey: Bool = false) {
-//        type(of: self).storeObject(self, id: ignoreKey ? nil : String(any: self.id))
-//    }
-//
-//    static func arrayStoreKey() -> String {
-//        return String(fullname: self) + "#array"
-//    }
-//
-//    static func objectStoreKey(id: String? = nil) -> String {
-//        guard let id = id else { return String(fullname: self) + "#object" }
-//        return String(fullname: self) + "#object#" + id
-//    }
-//
-//    static func storeObject(_ object: Self, id: String? = nil) {
-//        let key = self.objectStoreKey(id: id)
-//        try? storage.transformCodable(ofType: self).setObject(object, forKey: key)
-//    }
-//
-//    static func storeArray(_ array: Array<Self>) {
-//        let key = self.arrayStoreKey()
-//        try? storage.transformCodable(ofType: Array<Self>.self).setObject(array, forKey: key)
-//    }
-//
-//    static func cachedObject(id: String? = nil) -> Self? {
-//        let key = self.objectStoreKey(id: id)
-//        if let object = try? storage.transformCodable(ofType: self).object(forKey: key) {
-//            return object
-//        }
-//
-//        if let path = Bundle.main.path(forResource: key, ofType: "json"),
-//            let json = try? String(contentsOfFile: path, encoding: .utf8) {
-//            return Self(JSONString: json)
-//        }
-//
-//        return nil
-//    }
-//
-//    static func cachedArray() -> Array<Self>? {
-//        let key = self.arrayStoreKey()
-//        if let array = try? storage.transformCodable(ofType: Array<Self>.self).object(forKey: key) {
-//            return array
-//        }
-//
-//        if let path = Bundle.main.path(forResource: key, ofType: "json"),
-//            let json = try? String(contentsOfFile: path, encoding: .utf8) {
-//            return Array<Self>(JSONString: json)
-//        }
-//
-//        return nil
-//    }
-//
-//    static func eraseObject(id: String? = nil) {
-//        let key = self.objectStoreKey(id: id)
-//        try? storage.removeObject(forKey: key)
-//    }
-//
-//    static func == (lhs: Self, rhs: Self) -> Bool {
-//        return lhs.id == rhs.id
-//    }
-//}
-
-// MARK: - 流协议
-//public protocol Subjective {
-//    var subject: BehaviorRelay<ModelType?> { get }
-//}
-//
-//public extension Subjective {
-//
-//    var subject: BehaviorRelay<ModelType?> {
-//        return BehaviorRelay<ModelType?>(value: nil)
-//    }
-//
-//}
-
-//public protocol Subjective2: Storable2 {
-//    static func subject() -> BehaviorRelay<Self?>
-//    static func current() -> Self?
-//    static func update(_ value: Self?, _ toCache: Bool)
-//}
-//
-//public extension Subjective2 {
-//    static func subject() -> BehaviorRelay<Self?> {
-//        let key = String(fullname: self)
-//        if let subject = subjects2[key] as? BehaviorRelay<Self?> {
-//            return subject
-//        }
-//        let subject = BehaviorRelay<Self?>(value: Self.cachedObject())
-//        subjects2[key] = subject
-//        return subject
-//    }
-//
-//    static func current() -> Self? {
-//        self.subject().value
-//    }
-//
-//    static func update(_ value: Self?, _ toCache: Bool = true) {
-//        let subject = self.subject()
-//        guard let value = value else {
-//            if toCache {
-//                Self.eraseObject()
-//            }
-//            subject.accept(nil)
-//            return
-//        }
-//
-//        if toCache {
-//            Self.storeObject(value)
-//        }
-//        subject.accept(value)
-//    }
-//
-//}
 
