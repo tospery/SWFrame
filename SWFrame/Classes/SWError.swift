af141d5d7c4940083a637bca1c577540145b70b8
yangjianxiang <yangjianxiang@xunyou.com>
2021-01-05 16:14:37 +0800

    error

---
+++ SWFrame/Classes/SWError.swift
@@ -0,0 +1,53 @@
+//
+//  SWError.swift
+//  SWFrame
+//
+//  Created by liaoya on 2021/1/5.
+//
+
+import Foundation
+import RxSwift
+import Moya
+
+public protocol SWErrorCompatible {
+    var asSWError: SWError { get }
+}
+
+public enum SWError: Error {
+    case network(Int, String?)  // 100~199
+    case server(Int, String?)   // 200~299
+    case user(Int, String?)     // 300~399
+    case app(Int, String?)      // 400~499
+}
+
+extension SWError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case let .network(_, message):
+            return message ?? NSLocalizedString("Error.Network.Message", comment: "")
+        case let .server(_, message):
+            return message ?? NSLocalizedString("Error.Server.Message", comment: "")
+        case let .user(_, message):
+            return message
+        case let .app(_, message):
+            return message
+        }
+    }
+}
+
+extension SWError: Equatable {
+    public static func == (lhs: SWError, rhs: SWError) -> Bool {
+        switch (lhs, rhs) {
+        case let (.network(code1, _), .network(code2, _)):
+            return code1 == code2
+        case let (.server(code1, _), .server(code2, _)):
+            return code1 == code2
+        case let (.user(code1, _), .user(code2, _)):
+            return code1 == code2
+        case let (.app(code1, _), .app(code2, _)):
+            return code1 == code2
+        default:
+            return false
+        }
+    }
+}
