2642ccab0533287da39bd8cb3400bca020c142a0
yangjianxiang <yangjianxiang@xunyou.com>
2021-02-08 14:16:38 +0800

    sample

--- SWFrame/Classes/TargetTypeExt.swift
+++ SWFrame/Classes/TargetTypeExt.swift
@@ -9,12 +9,12 @@ import Moya
 
 public extension TargetType {
 
-    var samplePath: URL? {
-        var name = self.path.replacingOccurrences(of: "/", with: "-")
-        if name.hasPrefix("-") {
-            name = String.init(name[name.index(after: name.startIndex)...])
+    var samplePath: String? {
+        var path = self.path.replacingOccurrences(of: "/", with: "-")
+        if path.hasPrefix("-") {
+            path.removeFirst()
         }
-        return Bundle.main.url(forResource: name, withExtension: "json")
+        return path
     }
-    
+
 }
