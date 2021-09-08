74a817bfd74a961444f9b804b401c4e7d9106c85
yangjianxiang <yangjianxiang@xunyou.com>
2021-09-08 16:23:30 +0800

    状态栏

--- SWFrame/Classes/UIStatusBarStyle+Ex.swift
+++ SWFrame/Classes/UIStatusBarStyle+Ex.swift
@@ -11,8 +11,26 @@ public extension UIStatusBarStyle {
 
     var reversed: UIStatusBarStyle {
         switch self {
+        case .default:
+            if #available(iOS 12.0, *) {
+                switch UIScreen.main.traitCollection.userInterfaceStyle {
+                case .light:
+                    if #available(iOS 13.0, *) {
+                        return .darkContent
+                    } else {
+                        return .default
+                    }
+                default: return .lightContent
+                }
+            } else {
+                return .lightContent
+            }
         case .lightContent:
-            return .default
+            if #available(iOS 13.0, *) {
+                return .darkContent
+            } else {
+                return .default
+            }
         default:
             return .lightContent
         }
