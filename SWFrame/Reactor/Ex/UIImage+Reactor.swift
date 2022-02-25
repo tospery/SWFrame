92a8dde436a2814e09b53350968730dce39b564c
yangjianxiang <yangjianxiang@xunyou.com>
2022-02-25 17:16:42 +0800

    image

--- SWFrame/Reactor/Ex/UIImage+Reactor.swift
+++ SWFrame/Reactor/Ex/UIImage+Reactor.swift
@@ -7,14 +7,13 @@
 
 import UIKit
 
-
 public extension UIImage {
     
     fileprivate class func image(name: String) -> UIImage {
         var image = UIImage(named: name, in: Bundle.main, compatibleWith: nil)
         if image == nil {
             var bundle = Bundle(for: BaseView.self)
-            if let path = bundle.path(forResource: "SWFrame", ofType: "bundle") {
+            if let path = bundle.path(forResource: "Resources", ofType: "bundle") {
                 bundle = Bundle(path: path)!
             }
             image = UIImage(named: name, in: bundle, compatibleWith: nil)
@@ -28,39 +27,39 @@ public extension UIImage {
     }
     
     static var back: UIImage {
-        return self.image(name: "back")
+        return self.image(name: "ic_back")
     }
     
     static var close: UIImage {
-        return self.image(name: "close")
+        return self.image(name: "ic_close")
     }
     
     static var indicator: UIImage {
-        return self.image(name: "indicator")
+        return self.image(name: "ic_indicator")
     }
     
     static var loading: UIImage {
-        return self.image(name: "loading")
+        return self.image(name: "ic_loading")
     }
     
     static var waiting: UIImage {
-        return self.image(name: "waiting")
+        return self.image(name: "ic_waiting")
     }
     
     static var networkError: UIImage {
-        return self.image(name: "errorNetwork")
+        return self.image(name: "ic_error_network")
     }
     
     static var serverError: UIImage {
-        return self.image(name: "errorServer")
+        return self.image(name: "ic_error_server")
     }
     
     static var emptyError: UIImage {
-        return self.image(name: "errorEmpty")
+        return self.image(name: "ic_error_empty")
     }
     
     static var expireError: UIImage {
-        return self.image(name: "errorExpire")
+        return self.image(name: "ic_error_expire")
     }
     
 }
